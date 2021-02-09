8b616bf0cf2a37f38be36194f2d160a5fe3a8855
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-09 20:56:55 +0530

    Created a tictactoe board

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,23 @@ public class TicTacToe
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Tic Tac Toe Java");
-				
+		
+		char[] board = boardDesign();
+		System.out.println(board);
+		
+	}
+	
+	public static char[] boardDesign()
+	{
+		char[] board = new char[10];
+		board[0] = '#';                  // ignored to make it user friendly
+		
+		for(int i=1; i<10; i++)
+		{
+			board[i] = ' ';
+		}
+		
+		return board;
 	}
 }
 	
