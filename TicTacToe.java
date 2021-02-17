ee8624c4a697c38255b0fc6ea3cfd4f763333541
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-17 18:31:39 +0530

    Added a method to display board

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,24 +3,29 @@ import java.util.Scanner;
 
 public class TicTacToe 
 {
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to Tic Tac Toe Java");
 		
-		System.out.println("Player choose X or O");
+		System.out.println("Welcome to Tic Tac Toe Java\r");
+		
+		System.out.println("Player choose X or O\r");
 		char player = sc.next().charAt(0);
 		
 		char computer = chooseLetter(player);
-		System.out.println("Computer gets "+computer);
+		System.out.println("Computer gets \r"+computer);
+		
+		System.out.println("Let's start the game");
+		System.out.println("Current board is :");
+		showBoard();
 	}
 	
 	public static char[] boardDesign()
 	{
-		char[] board = new char[10];
-		board[0] = '#';                  // ignored to make it user friendly
-		
-		for(int i=1; i<10; i++)
+		char[] board = new char[10];     
+
+		for(int i=1; i<9; i++)
 		{
 			board[i] = ' ';
 		}
@@ -42,6 +47,23 @@ public class TicTacToe
 			return computer;
 		}
 	}
+	
+	public static void showBoard()
+	{
+		char[] board = boardDesign();
+		
+		
+		System.out.println("         |                    |           ");
+		System.out.println(board[7]+"        |          "+board[8]+"         |          "+board[9]);
+		System.out.println("---------|--------------------|-----------");
+	    System.out.println("         |                    |           ");
+	    System.out.println(board[4]+"        |          "+board[5]+"         |          "+board[6]);
+	    System.out.println("---------|--------------------|-----------");
+	    System.out.println("         |                    |           ");
+	    System.out.println(board[1]+"        |          "+board[2]+"         |          "+board[3]);
+	     
+	}
+	
 }
 	
 
